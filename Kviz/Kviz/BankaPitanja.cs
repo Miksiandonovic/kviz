c4efff270c8dfe9aa95b2b4379fcf2faa6a564ce
Miksiandonovic <miksiandonovic@gmail.com>
2024-12-11 16:06:48 +0100

    BankaPitanja/DodajPitanje Uradjeno

---
+++ Kviz/Kviz/BankaPitanja.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kviz
+{
+    class BankaPitanja
+    {
+        public List<Pitanje> Pitanja { get; private set; }
+
+        public BankaPitanja(List<Pitanje> p)
+        {
+            Pitanja = p;
+        }
+        public int maxID()
+        {
+            int a = Pitanja[0].Id;
+            for (int i = 1; i < Pitanja.Count; i++)
+            {
+                if (Pitanja[i].Id > a)
+                {
+                    a = Pitanja[i].Id;
+                }
+            }
+            return a;
+        }
+        void DodajPitanje(Pitanje Question)
+        {
+            Pitanja.Add(Question);
+            Question.Id = maxID();
+        }
+
+
+    }
+}
